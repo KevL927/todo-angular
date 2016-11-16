114863be5651d70c24319c48e53f49f36b3b24a3
KevL927 <leekevin@gmail.com>
2016-11-16 17:27:14 -0500

    Add edited tag on changed todo items

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,15 @@
 angular.module("todoListApp", [])
   .controller('mainCtrl', function($scope) {
     $scope.check = function() {
-    }
-    })
+    };
+
+    $scope.todos = [
+      {"name": "clean the house"},
+      {"name": "water the dog"},
+      {"name": "feed the lawn"},
+      {"name": "pay dem bills"},
+      {"name": "run"},
+      {"name": "swim"}
+    ]
+
   });
